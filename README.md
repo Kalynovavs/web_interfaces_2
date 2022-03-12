6e4d9f100139dc1ac8331fb721c6623b0f7a93de
kalynovavs <valentinka18.12.95@gmail.com>
2022-03-12 22:57:35 +0300

    add tests

---
+++ README.md
@@ -0,0 +1,1 @@
+# web_interfaces [![Build status](https://ci.appveyor.com/api/projects/status/653y61i59eltpmvw?svg=true)](https://ci.appveyor.com/project/Kalynovavs/web-interfaces-2)
---
+++ src/test/java/ru/netology/interfaces/CallbackTest.java
@@ -0,0 +1,181 @@
+package ru.netology.interfaces;
+
+import com.codeborne.selenide.SelenideElement;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CallbackTest {
+    private WebDriver driver;
+    private LocalDateTime dateNow;
+    private DateTimeFormatter dtf;
+    private String dateAllowable;
+
+    @BeforeAll
+    static void setUpAll() {
+        //System.setProperty("webdriver.chrome.driver", "webdriver//chromedriver");
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("disable-infobars");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--no-sandbox");
+        driver = new ChromeDriver(options);
+        open("http://localhost:9999");
+        dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        dateNow = LocalDateTime.now();
+        dateAllowable = dtf.format(dateNow.plusDays(4));
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    void shouldSubmitRequestSuccess() {
+        SelenideElement form =$(".form");
+        form.$("[data-test-id=city] input").setValue("Нижний Новгород");
+        form.$("[data-test-id=date] input").doubleClick();
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(dateAllowable);
+        form.$("[data-test-id=name] input").setValue("Василий Маршал-Передовой");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldFormValidateWithoutAgreement() {
+        SelenideElement form =$(".form");
+        form.$("[data-test-id=city] input").setValue("Нижний Новгород");
+        form.$("[data-test-id=date] input").doubleClick();
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(dateAllowable);
+        form.$("[data-test-id=name] input").setValue("Василий Маршал-Передовой");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$(".button").click();
+        form.$("[data-test-id=agreement]").shouldHave(cssClass("input_invalid"));
+    }
+
+    @Test
+    void shouldFormValidateEmptyName() {
+        SelenideElement form =$(".form");
+        form.$("[data-test-id=city] input").setValue("Нижний Новгород");
+        form.$("[data-test-id=date] input").doubleClick();
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(dateAllowable);
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        form.$("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldFormValidateEmptyNumber() {
+        SelenideElement form =$(".form");
+        form.$("[data-test-id=city] input").setValue("Нижний Новгород");
+        form.$("[data-test-id=date] input").doubleClick();
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(dateAllowable);
+        form.$("[data-test-id=name] input").setValue("Василий Маршал-Передовой");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        form.$("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldFormValidateWrongName() {
+        SelenideElement form =$(".form");
+        form.$("[data-test-id=city] input").setValue("Нижний Новгород");
+        form.$("[data-test-id=date] input").doubleClick();
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(dateAllowable);
+        form.$("[data-test-id=name] input").setValue("kalynova valentina");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        form.$("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldFormValidateWrongNumber() {
+        SelenideElement form =$(".form");
+        form.$("[data-test-id=city] input").setValue("Нижний Новгород");
+        form.$("[data-test-id=date] input").doubleClick();
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(dateAllowable);
+        form.$("[data-test-id=name] input").setValue("Василий Маршал-Передовой");
+        form.$("[data-test-id=phone] input").setValue("77779270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        form.$("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldFormValidateWrongDate() {
+        SelenideElement form =$(".form");
+        form.$("[data-test-id=city] input").setValue("Нижний Новгород");
+        form.$("[data-test-id=date] input").doubleClick();
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue("32");
+        form.$("[data-test-id=name] input").setValue("Василий Маршал-Передовой");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        form.$("[data-test-id=date] .input__sub").shouldHave(exactText("Неверно введена дата"));
+    }
+
+    @Test
+    void shouldFormValidateNotAllowedDate() {
+        SelenideElement form =$(".form");
+        form.$("[data-test-id=city] input").setValue("Нижний Новгород");
+        form.$("[data-test-id=date] input").doubleClick();
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(dtf.format(dateNow.plusDays(1)));
+        form.$("[data-test-id=name] input").setValue("Василий Маршал-Передовой");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        form.$("[data-test-id=date] .input__sub").shouldHave(exactText("Заказ на выбранную дату невозможен"));
+    }
+
+    @Test
+    void shouldFormValidateWrongCiti() {
+        SelenideElement form =$(".form");
+        form.$("[data-test-id=city] input").setValue("Несуществующий город");
+        form.$("[data-test-id=date] input").doubleClick();
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(dateAllowable);
+        form.$("[data-test-id=name] input").setValue("Василий Маршал-Передовой");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        form.$("[data-test-id=city].input_invalid .input__sub").shouldHave(exactText("Доставка в выбранный город недоступна"));
+    }
+}
